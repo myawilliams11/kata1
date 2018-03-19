6d3b97108a89ec1e107a0f84621e61a8e32b393e
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-03-19 15:56:08 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,73 @@
+// 1
+
+    for(let i = 1; i <= 20; i++) {
+      document.write(i + " ");
+    }
+ 
+
+// 2
+      for(let i = 2; i <= 20; i += 2) {
+        document.write(i + " ");
+      }
+   
+//  3
+      for(let i = 1; i <= 20; i += 2) {
+        document.write(i + " ");
+      }
+   
+
+      // 4
+      for(let i = 5; i <= 20; i += 5) {
+        document.write(i + " ");
+      }
+  
+      
+      // 5
+      for(let i = 1; i <= 10; i++) {
+        document.write((i * i) + ' ');
+      }
+  
+      
+      // 6
+      for(let i = 20; i >= 1; --i) {
+        document.write(i - " ");
+      }
+   
+      
+      // 7
+      for(let i = 20; i >= 1; i-=2 ) {
+        document.write(i - " ");
+      }
+   
+      
+      // 8
+      for(let i = 19; i >= 1; i-=2) {
+        document.write(i - " ");
+      }
+   
+      
+      // 9
+      for(let i = 100; i >= 1; i-=5) {
+        document.write(i - " ");
+      }
+   
+      
+      // 10
+      for(let i = 100; i >= 1; --i) {
+        document.write(i * i) (' ');
+        }
+
+      for(let i = 1; i <= 10; i++) {
+        document.write((i * i) + ' ');
+      }
+
+     
+      
+    //   // 11
+    const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
+
+    for (let i= 0; i <= sampleArray.length; i++ ){
+      document.write(( sampleArray[i]));
+    }
+
+
